a1a4a461024942a4c271b129c2a24d9b34893c84
vespindola <viniciusespindola3@gmail.com>
2020-05-12 22:23:05 -0300

    Fragments

---
+++ app/src/main/java/com/tads/fragments/activity/MainActivity.java
@@ -0,0 +1,59 @@
+package com.tads.fragments.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tads.fragments.R;
+import com.tads.fragments.fragment.ContatosFragment;
+import com.tads.fragments.fragment.ConversasFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConversasFragment conversasFragment;
+    private ContatosFragment contatosFragment;
+    private Button buttonConversas, buttonContatos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonContatos = findViewById(R.id.buttonContatos);
+        buttonConversas = findViewById(R.id.buttonConversas);
+
+        getSupportActionBar().setElevation(0);
+
+        conversasFragment = new ConversasFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameConteudo, conversasFragment);
+        transaction.commit();
+
+        buttonContatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contatosFragment = new ContatosFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, contatosFragment);
+                transaction.commit();
+            }
+        });
+
+        buttonConversas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conversasFragment = new ConversasFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, conversasFragment);
+                transaction.commit();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/tads/fragments/fragment/ContatosFragment.java
@@ -0,0 +1,37 @@
+package com.tads.fragments.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.tads.fragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContatosFragment extends Fragment {
+
+    private TextView textContato;
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        textContato = view.findViewById(R.id.textContato);
+        textContato.setText("Contatos Alterados!");
+
+        return view;
+    }
+}
